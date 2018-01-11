b4ad6d0bf9cd2309fe670ede09759f88463bcf69
danieljmarshall <35314815+danieljmarshall@users.noreply.github.com>
2018-01-11 15:13:36 -0500

    Update GUI.py
    Added rotate button.

--- GUI.py
+++ GUI.py
@@ -11,6 +11,9 @@ topFrame.place(x=0, y=0)
 controlsFrame = Frame(root, width=200, height=800, bg="Blue")
 controlsFrame.place(x=0, y=100)
 
+rotateButton = Button(controlsFrame, text="Rotate", fg="white", bg="RED", width=10, height = 2)
+rotateButton.place(x=66, y=5)
+
 titleLabel = Label(topFrame, text="PhotoNot CC 2018", fg="white", bg="green", font=("Impact", 40))
 titleLabel.place(x=5, y=15)
 exitButton = Button(topFrame, text="CLOSE", bg="RED", fg="WHITE", font="Impact", height=1, width=5, command=quit)
