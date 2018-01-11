f69613a8bdf2b84131711a1c615b5fed529eec22
peterdmarshall <32017006+peterdmarshall@users.noreply.github.com>
2018-01-10 21:05:39 -0500

    Test

--- ImageManip.py
+++ ImageManip.py
@@ -1,2 +1,15 @@
 import PIL
 import math
+
+class ImageManip:
+    def __init__(self, imageObj):
+        self.image = imageObj
+
+    def rotate(self, degrees):
+        self.image.rotate(degrees).expand()
+
+
+
+
+
+
