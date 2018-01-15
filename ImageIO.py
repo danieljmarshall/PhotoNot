69e83630caa5dc3754e4a897fec1f4acbcf69ec1
peterdmarshall <32017006+peterdmarshall@users.noreply.github.com>
2018-01-14 21:39:08 -0500

    ImageManip Methods

--- ImageIO.py
+++ ImageIO.py
@@ -1,1 +1,13 @@
-import PIL
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image
+
+root = Tk()
+root.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                           filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+# print(root.filename)
+im = Image.open(root.filename)
+im.show()
+input("Press Enter to Continue")
+
+root.filename = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
